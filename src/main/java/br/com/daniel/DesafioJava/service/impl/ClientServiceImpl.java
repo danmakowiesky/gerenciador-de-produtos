04830ae5c5b11cab99969f422e2840be1a0f6e18
danielmakowiesky <dvm1993@hotmail.com>
2020-06-18 09:37:36 -0300

    ajustando packages

--- src/main/java/br/com/daniel/DesafioJava/service/impl/ClientServiceImpl.java
+++ src/main/java/br/com/daniel/DesafioJava/service/impl/ClientServiceImpl.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.daniel.DesadioJava.repositories.ClientRepository;
 import br.com.daniel.DesafioJava.entity.Client;
+import br.com.daniel.DesafioJava.repositories.ClientRepository;
 import br.com.daniel.DesafioJava.service.ClientService;
 
 @Service
